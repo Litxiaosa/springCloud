7403b1586372d01a54538f8122f4521072bf1a6e
xiaosa <416082509@qq.com>
2017-12-25 15:59:06 +0800

    初始化注册中心。

---
+++ eureka-server/src/main/java/com/cloud/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * @author xiaosa
+ * EnableEurekaServer注解：启用服务注册中心，证明该服务是注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
