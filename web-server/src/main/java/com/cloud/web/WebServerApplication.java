9d128266bb20c9a0f5a5d7d5fb62c8d7c9f351b9
xiaosa <416082509@qq.com>
2018-01-02 11:25:39 +0800

    增加服务容错保护

--- web-server/src/main/java/com/cloud/web/WebServerApplication.java
+++ web-server/src/main/java/com/cloud/web/WebServerApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @MapperScan("com.cloud.web.dao")
 @SpringBootApplication
+@EnableHystrix
 public class WebServerApplication {
 
 	public static void main(String[] args) {
